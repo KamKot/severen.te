ccc70442b829579e49afc84f61d9dc8354a41b54
KamKot <ivan@guscorp.ru>
2016-02-28 18:41:13 +0300

    Скрипт с параметрами подключения

---
+++ config_connect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 27.02.16
+ * Time: 21:37
+ */
+//Данные для работы с MySQL
+//Сервер, в большинстве случаев менять не требуется
+$DBSERVER = "localhost";
+//Пользователь и пароль
+$DBUSER = "root";
+$DBPASS = "severen";
+//База данных
+$DB = "Severen_te";
+?>
